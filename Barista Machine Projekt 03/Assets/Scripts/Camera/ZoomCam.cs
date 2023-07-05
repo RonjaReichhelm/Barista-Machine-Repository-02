36a700facb3defa7d908c9e0cc44915e13599c18
RRonja <132659259+RonjaReichhelm@users.noreply.github.com>
2023-07-05 14:20:59 +0200

    settings repariert

--- Barista Machine Projekt 03/Assets/Scripts/Camera/ZoomCam.cs
+++ Barista Machine Projekt 03/Assets/Scripts/Camera/ZoomCam.cs
@@ -13,26 +13,19 @@ public class ZoomCam : MonoBehaviour
     public float zoom_limit_near = 20;
     public float zoom_limit_far = 20;
     public float zoom_speed = 2;
-
-    //This script handles the zooming in this demonstrator
-
-    // Start is called before the first frame update
     void Start()
     {
-
-        //  m_Lens = GetComponent<LensSettings>();
-
+        
         m_Camera = GetComponent<CinemachineVirtualCamera>();
         m_Camera.m_Lens.FieldOfView = 70;
         
     }
-
     private void LateUpdate()
     {
         zoomCamera();
         if (Input.GetMouseButtonDown(2)) // 2 steht für die mittlere Maustaste
         {
-            m_Camera.m_Lens.FieldOfView = 100;
+            m_Camera.m_Lens.FieldOfView = 70;
         }
     }
 
@@ -46,20 +39,20 @@ public class ZoomCam : MonoBehaviour
             if (wheel_Axis > 0 && (currentFOV - 1 > zoom_limit_far))
             {
                 m_Camera.m_Lens.FieldOfView -= zoom_speed;
-            }
-
+            } 
+            
             if (wheel_Axis < 0 && (currentFOV + 1 < zoom_limit_near))
             {
                 m_Camera.m_Lens.FieldOfView += zoom_speed ;
             } ;
+            
+            
         }
         
     }
     
-    // Update is called once per frame
     void Update()
     {
         zoomCamera();
-        // Debug.Log(Input.mouseScrollDelta.y);
     }
 }
